01999267fa4919d359b8ff9121218a704f1613c0
LCF2370 <c0921675@mylambton.ca>
2024-11-18 09:37:14 -0500

    Resolving issue with MongoDB Username

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,49 +5,48 @@ from app import flask_app
 import unittest
 
 # MongoDB mock test
-# from dotenv import load_dotenv
-# from pymongo import MongoClient
-# import pymongo
-# import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import pymongo
+import os
 
 #Read the .env file
-# load_dotenv()
-
+load_dotenv()
 flask_app.debug = True
 # Username and Password
-# db_username = os.environ["MONGODB_USERNAME"]
-# db_password = os.environ["MONGODB_PASSWORD"]
+db_username = os.environ["MONGODB_USERNAME"]
+db_password = os.environ["MONGODB_PASSWORD"]
 
 # Creates the Mongodb Client
 # mongodb_client = MongoClient(f"mongodb+srv://{db_username}:{db_password}@cluster0.dr7cs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Reference: https://www.mongodb.com/developer/products/mongodb/pytest-fixtures-and-pypi/
-# @pytest.fixture(scope="session")
-# def mongodb():
-#     client = pymongo.MongoClient(f"mongodb+srv://{db_username}:{db_password}@cluster0.dr7cs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-#     assert client.admin.command("ping")["ok"] != 0.0  # Check that the connection is okay.
-#     return client
-#
-# def test_mongodb_fixture(mongodb):
-#     """ This test will pass if MDB_URI is set to a valid connection string. """
-#     assert mongodb.admin.command("ping")["ok"] > 0
-#
-# @pytest.fixture
-# def rollback_session(mongodb):
-#     session = mongodb.start_session()
-#     session.start_transaction()
-#     try:
-#         yield session
-#     finally:
-#         session.abort_transaction()
-#
-# # Testing inserting data by adding new data and check in database if the is successful
-# def test_insert_mongodb(mongodb, rollback_session):
-#     new_data = {"name": "Pen", "tag": "Office Supply", "price": 23.99}
-#     db = mongodb["flask_app"]
-#     products_coll = db["products"]
-#     products_coll.insert_one(new_data, session=rollback_session,)
-#     assert (products_coll.find_one({"tag": "Office Supply"}, session=rollback_session)!= None)
+@pytest.fixture(scope="session")
+def mongodb():
+    client = pymongo.MongoClient(f"mongodb+srv://{db_username}:{db_password}@cluster0.dr7cs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    assert client.admin.command("ping")["ok"] != 0.0  # Check that the connection is okay.
+    return client
+
+def test_mongodb_fixture(mongodb):
+    """ This test will pass if MDB_URI is set to a valid connection string. """
+    assert mongodb.admin.command("ping")["ok"] > 0
+
+@pytest.fixture
+def rollback_session(mongodb):
+    session = mongodb.start_session()
+    session.start_transaction()
+    try:
+        yield session
+    finally:
+        session.abort_transaction()
+
+# Testing inserting data by adding new data and check in database if the is successful
+def test_insert_mongodb(mongodb, rollback_session):
+    new_data = {"name": "Pen", "tag": "Office Supply", "price": 23.99}
+    db = mongodb["flask_app"]
+    products_coll = db["products"]
+    products_coll.insert_one(new_data, session=rollback_session,)
+    assert (products_coll.find_one({"tag": "Office Supply"}, session=rollback_session)!= None)
 
 class FlaskTestCase(unittest.TestCase):
 
@@ -76,8 +75,8 @@ class FlaskTestCase(unittest.TestCase):
         print("Found data for: ", response.data)
         self.assertIn(b'Welcome to Dalmart', response.data)
 
-    # #Testing the connection of MongoDB
-    # def test_mongo_conn(self):
-    #     response = self.app.get("/mongo_conn")
-    #     print(response.json)
-    #     self.assertEqual(first=response.json, second={"status": "Successfully Connected to MongoDB"})
+    #Testing the connection of MongoDB
+    def test_mongo_conn(self):
+        response = self.app.get("/mongo_conn")
+        print(response.json)
+        self.assertEqual(first=response.json, second={"status": "Successfully Connected to MongoDB"})
